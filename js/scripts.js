1793ec6602654819839de92d1d274d8fe2cb8bf7
gigi ditz and In Mahaphol <in@>
2016-03-24 14:48:45 -0700

    Add Political Views project

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+  //Celebrity Dating Project
   $("form#about-you").submit(function(event){
     var age = parseInt($("input#age").val());
     var gender = $("select#gender").val();
@@ -22,6 +23,35 @@ $(document).ready(function(){
 
     } else {
       alert("Too young to date");
+    }
+
+    event.preventDefault();
+  });
+
+  //Political View Project
+  $("form#political-view").submit(function(event){
+    var answer1 = $("select#question1").val();
+    var answer2 = $("select#question2").val();
+
+    if (answer1 === "yes" && answer2 === "yes") {
+
+      $("#political-answer").empty().append("Pirate");
+      $("#political-result").show();
+
+    } else if (answer1 === "yes" && answer2 === "no") {
+
+      $("#political-answer").empty().append("Conservative");
+      $("#political-result").show();
+
+    } else if (answer1 === "no" && answer2 === "yes") {
+
+      $("#political-answer").empty().append("Liberal");
+      $("#political-result").show();
+
+    } else {
+      $("#political-answer").empty().append("going to need a little marijuana and answer the questions later");
+      $("#political-result").show();
+
     }
 
     event.preventDefault();
